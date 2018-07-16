26f0f5bfdeddca3d8ad1c2b601c8e10f2bcf9d80
DeliaStefania <delia.dedeaga@softvision.ro>
2018-07-16 16:38:27 +0300

    New application commits

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/RemindersLifecycleTest.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/RemindersLifecycleTest.java
@@ -4,6 +4,7 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
